875971f649970c0c22f6cdcdadc7d6ad45d30d3d
ionixftw <ionixftw@hotmail.com>
2018-09-07 01:01:35 +0600

    Add weird.java

---
+++ Weird.java
@@ -0,0 +1,27 @@
+public class Weird {
+    public static void main( String[] args ) {
+        System.out.println("Weird but valid java code for star pattern!!");
+        for ( int $$$ = 0; $$$ < 16; ++$$$ ) {
+            for ( int ___ = 0; ___ <= $$$ % 4; ++___ ) {
+                _o_(___);
+            }
+            _$_($$$);
+        }
+                                {;{      ;{;   ;;;;;
+                                ;{;      ;;}    ;;;
+                                ;};      };;    ;;;
+                                ;;;      ;;;    ;{;
+                                {};;;{;;;;;}    {};
+                                ;;;      ;;;    ;;;
+                                {;;      ;};    ;;}
+                                ;;;      ;};    ;;;
+                                {;}      ;;;   ;;;;;
+    }
+    public static void _o_(int _o_) {
+        char __$[] = {74,65,86,65};
+        System.out.printf(String.valueOf(__$[_o_]));
+    }
+    public static void _$_(int _$_) {
+        System.out.println();
+    }
+}
